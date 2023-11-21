8298d144316f9afb317123c4dcee7d15e1d96dff
Rayane Benamre <benamrerayane@proton.me>
2023-11-21 22:46:36 +0100

    Entities page responsiveness

--- src/routes/entities/+page.svelte
+++ src/routes/entities/+page.svelte
@@ -4,13 +4,15 @@
 	import { FacebookSolid, XCompanySolid } from 'flowbite-svelte-icons';
 </script>
 
-<header class="semi-screen flex w-full gap-8 bg-primary-900 p-16 drop-shadow-lg">
+<header
+	class="semi-screen-xl flex w-full flex-col gap-8 bg-primary-900 p-6 drop-shadow-lg xl:flex-row xl:p-16"
+>
 	<div class="flex h-full w-full flex-shrink flex-col drop-shadow-lg">
 		<Heading class="mb-6 text-slate-100">Meet Ben Shapiro...</Heading>
-		<div class="flex h-full w-full flex-row gap-8">
+		<div class="flex h-full w-full flex-col gap-8 xl:flex-row">
 			<a
 				href="/"
-				class="aspect-video h-full w-full bg-slate-300 transition-all hover:rounded-md hover:border-2 hover:border-red-700"
+				class="aspect-video w-full bg-slate-300 transition-all hover:rounded-md hover:border-2 hover:border-red-700 xl:h-full"
 			>
 			</a>
 			<div>
@@ -24,7 +26,7 @@
 					ipsa dolorem corporis neque atque explicabo numquam. Voluptate cumque ducimus eligendi
 					accusamus qui.
 				</P>
-				<div class="mt-8 flex gap-8">
+				<div class="mt-8 flex w-full justify-evenly gap-8 xl:w-auto xl:justify-normal">
 					<a href="/entities">
 						<Icon icon="mdi:instagram" class="h-10 w-10 text-slate-100" />
 					</a>
@@ -40,17 +42,17 @@
 	</div>
 </header>
 
-<main class="flex flex-col justify-evenly p-24">
+<main class="flex flex-col justify-evenly px-6 py-10 lg:p-24">
 	<Tabs
 		style="full"
-		defaultClass="flex rounded-lg divide-x divide-gray-200 shadow dark:divide-gray-700"
+		defaultClass="flex rounded-lg overflow-auto divide-x divide-gray-200 shadow dark:divide-gray-700"
 	>
 		{#each ['Individuals', 'Professionals', 'Organizations'] as entity, i (i)}
 			<TabItem open={i == 0} class="w-full">
-				<span slot="title" class="text-lg font-semibold">{entity}</span>
-				<div class="flex flex-wrap">
+				<span slot="title" class="text-base font-semibold lg:text-lg">{entity}</span>
+				<div class="flex flex-wrap justify-center">
 					{#each Array(5) as item, i (i)}
-						<div class="flex w-1/4 p-2">
+						<div class="flex w-64 p-2">
 							<a
 								href="/"
 								class="w-full flex-col gap-2 rounded-md border-[1px] border-gray-200 p-3 transition-all hover:bg-gray-100 hover:drop-shadow-lg"
