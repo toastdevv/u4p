77cfecaa62038053d5b2e84941f40756aeb2e2f3
Rayane Benamre <benamrerayane@proton.me>
2024-03-26 18:24:41 +0000

    Hopefully fixed build error

--- backup/blog/test.md
+++
@@ -1,9 +0,0 @@
----
-title: "Israel's record of telling lies"
-date: 'Wednesday, December 13th, 2023 at 6:16 PM UTC+1'
-author: 'Rayane Benamre'
-description: "For a while now, Israel has been very keen on spreading misinformation and lies while continuously getting debunked. Let's take a look at few of their lies."
-thumbnailUrl: ''
----
-
-Let's dive straight to the point.
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import { mdsvex } from 'mdsvex';
 
@@ -14,17 +14,7 @@ const config = {
 	],
 	extensions: ['.svelte', '.md'],
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			images: {
-				sizes: [640, 828, 1200, 1920, 3840],
-				formats: ['image/avif', 'image/webp'],
-				minimumCacheTTL: 300,
-				domains: ['freeps-xi.vercel.app', 'watermelon-toastdevv.vercel.app']
-			}
-		})
+		adapter: adapter()
 	}
 };
 
