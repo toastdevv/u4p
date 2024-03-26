cd1ac8425d2d0bfd5119d66ba9d3f63bcb0acda4
Rayane Benamre <benamrerayane@proton.me>
2024-03-26 18:37:38 +0000

    Fixed vercel integration!!

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import { mdsvex } from 'mdsvex';
 
@@ -14,8 +14,14 @@ const config = {
 	],
 	extensions: ['.svelte', '.md'],
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			images: {
+				sizes: [640, 828, 1200, 1920, 3840],
+				formats: ['image/avif', 'image/webp'],
+				minimumCacheTTL: 300,
+				domains: ['freeps-xi.vercel.app', 'watermelon-toastdevv.vercel.app']
+			}
+		})
 	}
-};
-
+}
 export default config;
