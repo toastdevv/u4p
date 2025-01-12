92ac56d21f1bff6612d5ddbfbd187ac1dd4c7f1e
Rayane Benamre <benamrerayane@proton.me>
2025-01-12 14:12:58 +0100

    Copyright year update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -185,7 +185,7 @@
 	</div>
 	<hr class="my-6 border-gray-200 dark:border-gray-700 sm:mx-auto lg:my-8" />
 	<div class="sm:flex sm:items-center sm:justify-between">
-		<p class="text-base text-gray-500">© 2023 - 2024 All Rights Reserved.</p>
+		<p class="text-base text-gray-500">© 2023 - 2025 All Rights Reserved.</p>
 	</div>
 </footer>
 
